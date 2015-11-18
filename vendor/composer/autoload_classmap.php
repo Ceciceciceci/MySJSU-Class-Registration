756ee1e02e88f642b5e373a5b456160bceab6aff
Edwin Limantara <edwin.limantara@yahoo.com>
2015-11-17 23:25:52 -0800

    Add view folders

--- vendor/composer/autoload_classmap.php
+++ vendor/composer/autoload_classmap.php
@@ -6,7 +6,10 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
+    'CreateCoursesTable' => $baseDir . '/database/migrations/2015_11_18_004252_create_courses_table.php',
+    'CreateInstructorsTable' => $baseDir . '/database/migrations/2015_11_18_004236_create_instructors_table.php',
     'CreatePasswordResetsTable' => $baseDir . '/database/migrations/2014_10_12_100000_create_password_resets_table.php',
+    'CreateStudentsTable' => $baseDir . '/database/migrations/2015_11_18_004225_create_students_table.php',
     'CreateUsersTable' => $baseDir . '/database/migrations/2014_10_12_000000_create_users_table.php',
     'DatabaseSeeder' => $baseDir . '/database/seeds/DatabaseSeeder.php',
     'File_Iterator' => $vendorDir . '/phpunit/php-file-iterator/src/Iterator.php',
