28a77f345405a205434f5f0cd2b9970e5dd1c24a
emmendoza1986 <emmendoza1986@gmail.com>
2015-11-17 23:22:46 -0800

    fixed professors controller

--- app/Http/Controllers/ProfessorsController.php
+++ app/Http/Controllers/ProfessorsController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
-class ProfessorController extends Controller
+class ProfessorsController extends Controller
 {
     /**
      * Display a listing of the resource.
