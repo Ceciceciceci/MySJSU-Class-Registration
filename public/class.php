4250b9706b4fa65d82eec6b3acc69d5a9206a7e3
emmendoza1986 <emmendoza1986@gmail.com>
2015-11-21 18:04:16 -0800

    Added CS Courses to Course Table Seeder

--- public/class.php
+++ public/class.php
@@ -11,13 +11,13 @@ DB::table('course')->insert(array(
     array('email' => 'dayle@example.com', 'votes' => 0),
 ));*/
 
-$csv = array_map('str_getcsv', file('test.csv'));
+$csv = array_map('str_getcsv', file('cs.csv'));
 $total = count($csv);
 //print_r($csv);
 $i=1;
 echo "DB::table('courses')->insert(array("."<br>";
 for($i; $i < $total; $i++){
-	echo "array('subject' => '".$csv[$i][0]."', 'courseNumber' => '".$csv[$i][1]."', 'courseName' => '".$csv[$i][2]."', 'class' => '".$csv[$i][3]."', 'section1' => '".$csv[$i][4]."', 'section2' => '".$csv[$i][5]."', 'days' => '".$csv[$i][6]."', 'startTime' => '".$csv[$i][7]."', 'endTime' => '".$csv[$i][8]."', 'room' => '".$csv[$i][9]."', 'instructor' => '".str_replace("'","''",$csv[$i][10])."', 'meetingDates' => '".$csv[$i][11]."'),"."<br>";
+	echo "array('subject' => '".$csv[$i][0]."', 'courseNumber' => '".$csv[$i][1]."', 'courseName' => '".$csv[$i][2]."', 'class' => '".$csv[$i][3]."', 'section1' => '".$csv[$i][4]."', 'section2' => '".$csv[$i][5]."', 'days' => '".$csv[$i][6]."', 'startTime' => '".$csv[$i][7]."', 'endTime' => '".$csv[$i][8]."', 'room' => '".$csv[$i][9]."', 'instructor' => '".addslashes($csv[$i][10])."', 'meetingDates' => '".$csv[$i][11]."'),"."<br>";
 	/*
 	for($j=0; $j < 12; $j++){
 		echo$csv[$i][$j];
