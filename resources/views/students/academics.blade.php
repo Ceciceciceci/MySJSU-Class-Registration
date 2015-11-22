9556b7b03011ac8aaa60a6c7ef1062b298c950b0
Maninderpal Singh <maninderpalsingh@Maninderpals-MacBook-Pro.local>
2015-11-21 20:20:47 -0800

    add graph to acdemic tab

--- resources/views/students/academics.blade.php
+++ resources/views/students/academics.blade.php
@@ -40,46 +40,35 @@
             <hr />
         </div>
         <div class="col-sm-9">
-            <canvas id="canvas"></canvas>
-            <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/1.0.2/Chart.min.js"></script>
-            <script type="text/javascript">
-                var lineData = {
-                    labels: ['Data 1', 'Data 2', 'Data 3', 'Data 4',
-                        'Data 5', 'Data 6', 'Data 7'],
-                    datasets: [{
-                        fillColor: 'rgba(0,0,0,0)',
-                        strokeColor: 'rgba(220,180,0,1)',
-                        pointColor: 'rgba(220,180,0,1)',
-                        data: [20, 30, 80, 20, 40, 10, 60]
-                    }, {
-                        fillColor: 'rgba(0,0,0,0)',
-                        strokeColor: 'rgba(151,187,205,1)',
-                        pointColor: 'rgba(151,187,205,1)',
-                        data: [60, 10, 40, 30, 80, 30, 20]
-                    }]
-                };
+            <div class="content-panel">
+                              <h4><i class="fa fa-angle-right"></i> GPA Chart</h4>
+                              <div class="panel-body text-center">
+                                  <canvas id="line" height="300" width="600"></canvas>
+                              </div>
+                          </div>
+            
+            
+            <script type="text/javascript" src="{{ URL::asset('js/Chart.min.js') }}"></script>
+            <script>  
+    var Script = function () {
+        var lineChartData = {
+            labels : ["Sem 1","Sem 2","Sem 3","Sem 4","Sem 5","Sem 6","Sem 7"],
+            datasets : [
+                {
+                    data : [3.5,4.0,4.0,3.5,3.75,3.2,4.0],
+                    fillColor : "rgba(151,187,205,0.5)",
+                    strokeColor : "rgba(151,187,205,1)",
+                    pointColor : "rgba(151,187,205,1)",
+                    pointStrokeColor : "#fff"
+                }
+            ]
 
-                Chart.defaults.global = {
-                    animationSteps : 50,
-                    tooltipYPadding : 16,
-                    tooltipCornerRadius : 0,
-                    tooltipTitleFontStyle : 'normal',
-                    tooltipFillColor : 'rgba(0,160,0,0.8)',
-                    animationEasing : 'easeOutBounce',
-                    scaleLineColor : 'black',
-                    scaleFontSize : 16,
-                    responsive : true
-                };
-
-                var ctx = document.getElementById('canvas').getContext('2d');
-                var lineDemo = new Chart(ctx).Line(lineData, {
-                    responsive: true,
-                    pointDotRadius: 10,
-                    bezierCurve: false,
-                    scaleShowVerticalLines: false,
-                    scaleGridLineColor: 'black'
-                });
-            </script>
+        };
+         new Chart(document.getElementById("line").getContext("2d")).Line(lineChartData);
+        }();
+    </script>
         </div>
+
+        
     </div>
 @endsection
