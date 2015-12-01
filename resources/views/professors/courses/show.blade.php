dfa696bb305ec84bd318630076b2b08f494c3c3e
Limantara <edwin@limantara.me>
2015-11-30 19:19:43 -0800

    Add professors/courses/show route

---
+++ resources/views/professors/courses/show.blade.php
@@ -0,0 +1,39 @@
+@extends('base')
+
+@section('navbar-right')
+    <ul class="nav navbar-nav navbar-right">
+        <li>
+            <a href="javascript:void(0)" style="color: white;cursor: default;">{{ Auth::user()->name }}</a>
+        </li>
+        <li>
+            <a href="{{ action('SiteController@logout') }}" class="sjsu-secondary" style="color: white;">Sign out</a>
+        </li>
+    </ul>
+@endsection
+
+@section('main')
+    <div class="row mysjsu-main-row">
+        <div class="col-sm-3">
+            <h4 class="lead">Quick Links</h4>
+            <hr />
+            <ul class="nav nav-pills nav-stacked">
+                <li>
+                    <a href="{{ action('CoursesController@index') }}">
+                        <i class="glyphicon glyphicon-search"></i>
+                        Search Classes
+                    </a>
+                </li>
+                <li>
+                    <a href="{{ action('CoursesController@addCode') }}">
+                        <i class="glyphicon glyphicon-list-alt"></i>
+                        Add Codes
+                    </a>
+                </li>
+            </ul>
+            <hr />
+        </div>
+        <div class="col-sm-9">
+
+        </div>
+    </div>
+@endsection
