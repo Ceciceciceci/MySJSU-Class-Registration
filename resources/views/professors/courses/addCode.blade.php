0af8dd9d42c32371584ad816c619ef59402266cd
Maninderpal Singh <maninderpalsingh@Maninderpals-MacBook-Pro.local>
2015-12-02 21:21:55 -0800

    add code

--- resources/views/professors/courses/addCode.blade.php
+++ resources/views/professors/courses/addCode.blade.php
@@ -40,6 +40,69 @@
         </div>
         <div class="col-sm-9">
 
+            <h4 class="lead">Genrate Add Code</h4>
+            <hr />
+
+            <table  class="table table-bordered table-striped">
+                <thead >
+                <tr>
+                    
+                    <th style = "text-align:center;"><button onclick="genrate()">Genrate Code</button></th>
+                    <th> Important info </th>
+
+                    <script>
+
+                        var a =  1;
+
+                        var code = 10000;
+
+                        var code1 = "<br>";
+                        
+                        function genrate (e) {     
+
+                            document.getElementById("demo").innerHTML += code + "<br>";
+
+                            code = code + a;
+
+                            return false;
+
+                        }
+                    </script>
+                    
+                </tr>
+                </thead>
+                <tbody>
+               
+                    <tr>
+                        
+                        <td>
+                            <div align="center"  id= "genrateCode">
+                                <p id="demo">   </p> 
+                             
+                            </div>
+                        </td>
+
+                        <td> <div>
+                            <p> <b> This add code only given by professor to student privately <br>
+                                    so no any other student can use this code. Before give add<br> 
+                                    code to student just make sure <li>student are not on university
+                                    or major probation</li>
+                                    <li> Student met all of the pre-requisites </li>
+                                    <li>Student are not repeating the course</li>
+                                    
+
+                                </b>
+                            </p>
+
+                        </div>
+                        </td>
+                      
+                    </tr>
+
+             
+                </tbody>
+            </table>
+           
         </div>
     </div>
 @endsection
